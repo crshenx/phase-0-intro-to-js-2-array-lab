7132f15be2c94b0f17551a9760e85ab9c2690e9c
Chris Hendricks <crshenx@gmail.com>
2022-06-07 16:56:59 -0700

    commit tree fc23c1942b1b36b71e4146a656a54edadc7ac9b8

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  return cats;
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+  return cats;
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  let newCats = cats.slice();
+  newCats.push(name);
+  return newCats;
+}
+
+function prependCat(name) {
+  let newCats = cats.slice();
+  newCats.unshift(name);
+  return newCats;
+}
+
+function removeLastCat() {
+  let newCats = cats.slice();
+  newCats.pop();
+  return newCats;
+}
+
+function removeFirstCat() {
+  let newCats = cats.slice();
+  newCats.shift();
+  return newCats;
+}
